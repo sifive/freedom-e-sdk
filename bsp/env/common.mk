323a0e56dd7efc494557d43e96546a0b9e28d214
Yunsup Lee <yunsup@sifive.com>
2016-11-25 19:55:13 -0800

    add dhrystone

--- bsp/env/common.mk
+++ bsp/env/common.mk
@@ -15,29 +15,28 @@ INCLUDES += -I$(BSP_BASE)/include
 INCLUDES += -I$(ENV_DIR)
 INCLUDES += -I$(PLATFORM_DIR)
 
-CC = $(BSP_BASE)/../toolchain/bin/riscv32-unknown-elf-gcc
+CC := $(BSP_BASE)/../toolchain/bin/riscv32-unknown-elf-gcc
 
-LDFLAGS := -T $(LINKER_SCRIPT) -nostdlib -nostartfiles -lc -lgcc
+LDFLAGS += -T $(LINKER_SCRIPT) -nostdlib -nostartfiles -lc -lgcc
 LDFLAGS += -L$(ENV_DIR)
 
 ASM_OBJS := $(patsubst %.S,%.o,$(ASM_SRCS))
 C_OBJS := $(patsubst %.c,%.o,$(C_SRCS))
 
-ASM_STRIP_OBJS := $(foreach f,$(ASM_OBJS),$(notdir $f))
-C_STRIP_OBJS := $(foreach f,$(C_OBJS),$(notdir $f))
+LINK_OBJS += $(ASM_OBJS) $(C_OBJS)
 
-$(TARGET): $(ASM_OBJS) $(C_OBJS) $(LINKER_SCRIPT)
-	$(CC) $(CFLAGS) $(INCLUDES) $(ASM_STRIP_OBJS) $(C_STRIP_OBJS) -o $@ $(LDFLAGS)
+$(TARGET): $(LINK_OBJS) $(LINKER_SCRIPT)
+	$(CC) $(CFLAGS) $(INCLUDES) $(LINK_OBJS) -o $@ $(LDFLAGS)
 
-$(ASM_OBJS): %.o: %.S
-	$(CC) $(CFLAGS) $(INCLUDES) -c -o $(notdir $@) $<
+$(ASM_OBJS): %.o: %.S $(HEADERS)
+	$(CC) $(CFLAGS) $(INCLUDES) -c -o $@ $<
 
-$(C_OBJS): %.o: %.c
-	$(CC) $(CFLAGS) $(INCLUDES) -c -o $(notdir $@) $<
+$(C_OBJS): %.o: %.c $(HEADERS)
+	$(CC) $(CFLAGS) $(INCLUDES) -c -o $@ $<
 
 all: $(TARGET)
 
 clean:
-	rm -f $(TARGET) *.o
+	rm -f $(TARGET) $(LINK_OBJS)
 
 .PHONY: all clean
