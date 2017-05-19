bdd56ffea41bb1f27286fbc5f6a2cd445d853877
Drew Barbier <drew@sifive.com>
2017-05-19 13:48:14 -0500

    HiFive1 config tweaks

--- software/FreeRTOSv9.0.0/Demo/RISCV_HiFive1_GCC/FreeRTOSConfig.h
+++ software/FreeRTOSv9.0.0/Demo/RISCV_HiFive1_GCC/FreeRTOSConfig.h
@@ -80,8 +80,8 @@ your application. */
 #define configCPU_CLOCK_HZ                      265000000
 #define configRTC_CLOCK_HZ						32768
 #define configTICK_RATE_HZ                      250
-#define configMAX_PRIORITIES                    5
-#define configMINIMAL_STACK_SIZE                350
+#define configMAX_PRIORITIES                    3
+#define configMINIMAL_STACK_SIZE                400
 #define configMAX_TASK_NAME_LEN                 16
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
