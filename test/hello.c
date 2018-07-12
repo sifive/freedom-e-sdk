7483e801b474bfc21dbeeb2a3d2daef4b9b0edd3
Palmer Dabbelt <palmer@sifive.com>
2018-07-12 10:40:02 -0700

    Use printf() instead of calling the MEE directly
    This is a significantly better test of the MEE.  It also adds a weak
    __register_frame_info, which I think might just be working around a bug
    in the MEE toolchain.
    
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

--- test/hello.c
+++ test/hello.c
@@ -1,20 +1,8 @@
 /* Copyright 2018 SiFive, Inc */
 /* SPDX-License-Identifier: Apache-2.0 */
 
-#include <mee/tty.h>
+#include <stdio.h>
 
 void main() {
-    mee_tty_putc('H');
-    mee_tty_putc('e');
-    mee_tty_putc('l');
-    mee_tty_putc('l');
-    mee_tty_putc('o');
-    mee_tty_putc(' ');
-    mee_tty_putc('W');
-    mee_tty_putc('o');
-    mee_tty_putc('r');
-    mee_tty_putc('l');
-    mee_tty_putc('d');
-    mee_tty_putc('!');
-    mee_tty_putc('\n');
+    printf("Hello, World\n");
 }
