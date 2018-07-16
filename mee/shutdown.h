8d82a79d23bd1d1fbe67492f214873aae7dc545b
Palmer Dabbelt <palmer@sifive.com>
2018-07-16 13:53:38 -0700

    Add a driver for the SiFive test finisher
    This allows us to shut down machines that have hardware that allows them
    to be shut down.
    
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

--- mee/shutdown.h
+++ mee/shutdown.h
@@ -4,6 +4,19 @@
 #ifndef MEE__SHUTDOWN_H
 #define MEE__SHUTDOWN_H
 
+struct mee_shutdown;
+
+struct mee_shutdown_vtable {
+    void (*exit)(const struct mee_shutdown *sd, int code) __attribute__((noreturn));
+};
+
+struct mee_shutdown {
+    const struct mee_shutdown_vtable *vtable;
+};
+
+static inline void mee_shutdown_exit(const struct mee_shutdown *sd, int code) __attribute__((noreturn));
+static inline void mee_shutdown_exit(const struct mee_shutdown *sd, int code) { sd->vtable->exit(sd, code); }
+
 /* The public MEE shutdown interface, which allows us to turn off the machine
  * when posible. */
 void mee_shutdown(int code) __attribute__((noreturn));
