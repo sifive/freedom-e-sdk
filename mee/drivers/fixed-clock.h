f8bfcc09aa7cf18df9ff549160e90ba0ec9d1262
Palmer Dabbelt <palmer@sifive.com>
2018-07-16 22:29:21 -0700

    Move the fixed-clock driver's functions to its C file
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

--- mee/drivers/fixed-clock.h
+++ mee/drivers/fixed-clock.h
@@ -13,8 +13,8 @@ struct __mee_driver_vtable_fixed_clock {
     struct __mee_clock_vtable clock;
 };
 
-inline long __mee_driver_fixed_clock_get_rate_hz(const struct mee_clock *gclk);
-inline long __mee_driver_fixed_clock_set_rate_hz(struct mee_clock *gclk, long target_hz);
+long __mee_driver_fixed_clock_get_rate_hz(const struct mee_clock *gclk);
+long __mee_driver_fixed_clock_set_rate_hz(struct mee_clock *gclk, long target_hz);
 
 MEE_DECLARE_VTABLE(__mee_driver_vtable_fixed_clock) = {
     .clock.get_rate_hz = __mee_driver_fixed_clock_get_rate_hz,
@@ -22,23 +22,9 @@ MEE_DECLARE_VTABLE(__mee_driver_vtable_fixed_clock) = {
 };
 
 struct __mee_driver_fixed_clock {
-    const struct __mee_driver_vtable_fixed_clock *vtable;
     struct mee_clock clock;
+    const struct __mee_driver_vtable_fixed_clock *vtable;
     long rate;
 };
-
-
-inline long __mee_driver_fixed_clock_get_rate_hz(const struct mee_clock *gclk)
-{
-    const struct __mee_driver_fixed_clock *clk = (void *)gclk;
-    return clk->rate;
-}
-
-inline long __mee_driver_fixed_clock_set_rate_hz(struct mee_clock *gclk, long target_hz)
-{
-    const struct __mee_driver_fixed_clock *clk = (void *)gclk;
-    return clk->rate;
-}
-
 
 #endif
