sifive/freedom-metal

Move struct mee_shutdown to the private interface
Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

mee/shutdown.h
@@ -4,18 +4,18 @@
 #ifndef MEE__SHUTDOWN_H
 #define MEE__SHUTDOWN_H
 
-struct mee_shutdown;
+struct __mee_shutdown;
 
-struct mee_shutdown_vtable {
-    void (*exit)(const struct mee_shutdown *sd, int code) __attribute__((noreturn));
+struct __mee_shutdown_vtable {
+    void (*exit)(const struct __mee_shutdown *sd, int code) __attribute__((noreturn));
 };
 
-struct mee_shutdown {
-    const struct mee_shutdown_vtable *vtable;
+struct __mee_shutdown {
+    const struct __mee_shutdown_vtable *vtable;
 };
 
-inline void mee_shutdown_exit(const struct mee_shutdown *sd, int code) __attribute__((noreturn));
-inline void mee_shutdown_exit(const struct mee_shutdown *sd, int code) { sd->vtable->exit(sd, code); }
+inline void __mee_shutdown_exit(const struct mee_shutdown *sd, int code) __attribute__((noreturn));
+inline void __mee_shutdown_exit(const struct mee_shutdown *sd, int code) { sd->vtable->exit(sd, code); }
 
 /* The public MEE shutdown interface, which allows us to turn off the machine
  * when posible. */

sifive/freedom-e-sdk

mee/shutdown.h
@@ -4,18 +4,18 @@
 #ifndef MEE__SHUTDOWN_H
 #define MEE__SHUTDOWN_H
 
-struct mee_shutdown;
+struct __mee_shutdown;
 
-struct mee_shutdown_vtable {
-    void (*exit)(const struct mee_shutdown *sd, int code) __attribute__((noreturn));
+struct __mee_shutdown_vtable {
+    void (*exit)(const struct __mee_shutdown *sd, int code) __attribute__((noreturn));
 };
 
-struct mee_shutdown {
-    const struct mee_shutdown_vtable *vtable;
+struct __mee_shutdown {
+    const struct __mee_shutdown_vtable *vtable;
 };
 
-inline void mee_shutdown_exit(const struct mee_shutdown *sd, int code) __attribute__((noreturn));
-inline void mee_shutdown_exit(const struct mee_shutdown *sd, int code) { sd->vtable->exit(sd, code); }
+inline void __mee_shutdown_exit(const struct __mee_shutdown *sd, int code) __attribute__((noreturn));
+inline void __mee_shutdown_exit(const struct __mee_shutdown *sd, int code) { sd->vtable->exit(sd, code); }
 
 /* The public MEE shutdown interface, which allows us to turn off the machine
  * when posible. */