a7776c5229033cc16ccbc7a60bce9a017ac60caf
Bunnaroath Sou <bsou@sifive.com>
2018-12-17 18:33:30 -0800

    Update mee.h files

--- bsp/coreip-e31-arty/mee.h
+++ bsp/coreip-e31-arty/mee.h
@@ -1,7 +1,42 @@
 #ifndef ASSEMBLY
+
+#ifndef COREIP_E31_ARTY__MEE_H
+#define COREIP_E31_ARTY__MEE_H
+
+#define __MEE_CLINT_2000000_INTERRUPTS 		2
+#define MEE_MAX_CLINT_INTERRUPTS	 __MEE_CLINT_2000000_INTERRUPTS
+
+#define __MEE_INTERRUPT_CONTROLLER_C000000_INTERRUPTS 		1
+#define MEE_MAX_PLIC_INTERRUPTS	 __MEE_INTERRUPT_CONTROLLER_C000000_INTERRUPTS
+
+#define __MEE_GPIO_20002000_INTERRUPTS 		16
+#define MEE_MAX_GPIO_INTERRUPTS	 __MEE_GPIO_20002000_INTERRUPTS
+
+#define __MEE_SERIAL_20000000_INTERRUPTS 		1
+#define MEE_MAX_UART_INTERRUPTS	 __MEE_SERIAL_20000000_INTERRUPTS
+
+#include <mee/drivers/riscv,cpu.h>
+#include <mee/drivers/riscv,clint0.h>
+#include <mee/drivers/riscv,plic0.h>
 #include <mee/drivers/sifive,gpio0.h>
 #include <mee/drivers/sifive,uart0.h>
 #include <mee/drivers/sifive,test0.h>
+/* From cpu@0 */
+asm (".weak __mee_dt_cpu_0");
+struct __mee_driver_cpu __mee_dt_cpu_0;
+
+/* From clint@2000000 */
+asm (".weak __mee_dt_clint_2000000");
+struct __mee_driver_riscv_clint0 __mee_dt_clint_2000000;
+
+/* From interrupt_controller@c000000 */
+asm (".weak __mee_dt_interrupt_controller_c000000");
+struct __mee_driver_riscv_plic0 __mee_dt_interrupt_controller_c000000;
+
+/* From interrupt_controller */
+asm (".weak __mee_dt_interrupt_controller");
+struct __mee_driver_riscv_cpu_intc __mee_dt_interrupt_controller;
+
 /* From gpio@20002000 */
 asm (".weak __mee_dt_gpio_20002000");
 struct __mee_driver_sifive_gpio0 __mee_dt_gpio_20002000;
@@ -14,11 +49,95 @@ struct __mee_driver_sifive_uart0 __mee_dt_serial_20000000;
 asm (".weak __mee_dt_teststatus_4000");
 struct __mee_driver_sifive_test0 __mee_dt_teststatus_4000;
 
+/* From cpu@0 */
+struct __mee_driver_cpu __mee_dt_cpu_0 = {
+    .vtable = &__mee_driver_vtable_cpu,
+    .cpu.vtable = &__mee_driver_vtable_cpu.cpu_vtable,
+    .timebase = 1000000UL,
+    .interrupt_controller = &__mee_dt_interrupt_controller.controller,
+};
+
+/* From cpu@0 */
+#define __MEE_DT_RISCV_CPU_HANDLE (&__mee_dt_cpu_0.cpu)
+
+#define __MEE_DT_CPU_0_HANDLE (&__mee_dt_cpu_0.cpu)
+
+/* From interrupt_controller */
+struct __mee_driver_riscv_cpu_intc __mee_dt_interrupt_controller = {
+    .vtable = &__mee_driver_vtable_riscv_cpu_intc,
+    .controller.vtable = &__mee_driver_vtable_riscv_cpu_intc.controller_vtable,
+    .init_done = 0,
+    .interrupt_controller = 1,
+};
+
+/* From interrupt_controller */
+#define __MEE_DT_RISCV_CPU_INTC_HANDLE (&__mee_dt_interrupt_controller.controller)
+
+#define __MEE_DT_INTERRUPT_CONTROLLER_HANDLE (&__mee_dt_interrupt_controller.controller)
+
+/* From clint@2000000 */
+struct __mee_driver_riscv_clint0 __mee_dt_clint_2000000 = {
+    .vtable = &__mee_driver_vtable_riscv_clint0,
+    .clint.vtable = &__mee_driver_vtable_riscv_clint0.clint_vtable,
+    .control_base = 33554432UL,
+    .control_size = 65536UL,
+    .next_time_irq = 0,
+    .init_done = 0,
+    .interrupt_parent = &__mee_dt_interrupt_controller.controller,
+    .interrupt_lines[0] = 3,
+    .interrupt_lines[1] = 7,
+};
+
+/* From clint@2000000 */
+#define __MEE_DT_RISCV_CLINT0_HANDLE (&__mee_dt_clint_2000000.clint)
+
+#define __MEE_DT_CLINT_2000000_HANDLE (&__mee_dt_clint_2000000.clint)
+
+/* From interrupt_controller@c000000 */
+struct __mee_driver_riscv_plic0 __mee_dt_interrupt_controller_c000000 = {
+    .vtable = &__mee_driver_vtable_riscv_plic0,
+    .plic0.vtable = &__mee_driver_vtable_riscv_plic0.controller_vtable,
+    .init_done = 0,
+/* From interrupt_controller */
+    .interrupt_parent = &__mee_dt_interrupt_controller.controller,
+    .interrupt_line = 11UL,
+    .control_base = 201326592UL,
+    .control_size = 67108864UL,
+    .max_priority = 7UL,
+    .num_interrupts = 26UL,
+    .interrupt_controller = 1,
+};
+
+#define __MEE_DT_RISCV_PLIC0_NUM_INTRS 26
+
+/* From interrupt_controller@c000000 */
+#define __MEE_DT_RISCV_PLIC0_HANDLE (&__mee_dt_interrupt_controller_c000000.plic0)
+
+#define __MEE_DT_INTERRUPT_CONTROLLER_C000000_HANDLE (&__mee_dt_interrupt_controller_c000000.plic0)
+
 /* From gpio@20002000 */
 struct __mee_driver_sifive_gpio0 __mee_dt_gpio_20002000 = {
     .vtable = &__mee_driver_vtable_sifive_gpio0,
     .base = 536879104UL,
     .size = 4096UL,
+/* From interrupt_controller@c000000 */
+    .interrupt_parent = &__mee_dt_interrupt_controller_c000000.plic0,
+    .interrupt_lines[0] = 7,
+    .interrupt_lines[1] = 8,
+    .interrupt_lines[2] = 9,
+    .interrupt_lines[3] = 10,
+    .interrupt_lines[4] = 11,
+    .interrupt_lines[5] = 12,
+    .interrupt_lines[6] = 13,
+    .interrupt_lines[7] = 14,
+    .interrupt_lines[8] = 15,
+    .interrupt_lines[9] = 16,
+    .interrupt_lines[10] = 17,
+    .interrupt_lines[11] = 18,
+    .interrupt_lines[12] = 19,
+    .interrupt_lines[13] = 20,
+    .interrupt_lines[14] = 21,
+    .interrupt_lines[15] = 22,
 };
 
 /* From serial@20000000 */
@@ -29,6 +148,9 @@ struct __mee_driver_sifive_uart0 __mee_dt_serial_20000000 = {
     .control_size = 4096UL,
     .clock = NULL,
     .pinmux = NULL,
+/* From interrupt_controller@c000000 */
+    .interrupt_parent = &__mee_dt_interrupt_controller_c000000.plic0,
+    .interrupt_line = 5UL,
 };
 
 /* From teststatus@4000 */
@@ -41,7 +163,22 @@ struct __mee_driver_sifive_test0 __mee_dt_teststatus_4000 = {
 
 /* From teststatus@4000 */
 #define __MEE_DT_SHUTDOWN_HANDLE (&__mee_dt_teststatus_4000.shutdown)
+
+#define __MEE_DT_TESTSTATUS_4000_HANDLE (&__mee_dt_teststatus_4000.shutdown)
+
 /* From serial@20000000 */
 #define __MEE_DT_STDOUT_UART_HANDLE (&__mee_dt_serial_20000000.uart)
+
+#define __MEE_DT_SERIAL_20000000_HANDLE (&__mee_dt_serial_20000000.uart)
+
 #define __MEE_DT_STDOUT_UART_BAUD 115200
+
+#define __MEE_DT_MAX_HARTS 1
+
+asm (".weak __mee_cpu_table");
+struct __mee_driver_cpu *__mee_cpu_table = {
+					&__mee_dt_cpu_0};
+
+#endif /*MEE__MACHINE__COREIP_E31_ARTY__MEE_H*/
+
 #endif/*ASSEMBLY*/
