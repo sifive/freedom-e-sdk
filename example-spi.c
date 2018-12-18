574dfc1464cdb6fc12e08709bfd105117822accc
Nathaniel Graff <nathaniel.graff@sifive.com>
2018-12-18 10:27:49 -0800

    Update SPI example for driver fixes

--- example-spi.c
+++ example-spi.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 
 #include <mee/spi.h>
-#include <mee/machine.h>
-
-/* Choose SPI 1 if available, else choose SPI 0 */
-#ifdef __MEE_DT_SPI_1_HANDLE
-#define SPI_HANDLE __MEE_DT_SPI_1_HANDLE
-#elif __MEE_DT_SPI_0_HANDLE
-#define SPI_HANDLE __MEE_DT_SPI_0_HANDLE
-#else
-#error "No SPI device handle is provided by mee/machine.h"
-#endif
 
 int main() {
 	printf("MEE SPI Driver Demo\n");
 
+	struct mee_spi *spi;
+
+	/* Get SPI 1 */
+	spi = mee_spi_get_device(1);
+
+	/* Fallback to SPI 0 */
+	if(spi == NULL) {
+		spi = mee_spi_get_device(0);
+	}
+
+	if(spi == NULL) {
+		printf("Failed to get spi device\n");
+		return 1;
+	}
+
 	/* Initialize the SPI device to 100_000 baud */
-	mee_spi_init(SPI_HANDLE, 100000);
+	mee_spi_init(spi, 100000);
 
 	/* CPOL = 0, CPHA = 0, MSB-first, CS active low */
 	struct mee_spi_config config = {
-		.protocol = SPI_SINGLE,
+		.protocol = MEE_SPI_SINGLE,
 		.polarity = 0,
 		.phase = 0,
 		.little_endian = 0,
@@ -32,7 +37,7 @@ int main() {
 	char tx_buf[3] = {0x55, 0xAA, 0xA5};
 	char rx_buf[3] = {0};
 
-	mee_spi_transfer(SPI_HANDLE, &config, 3, tx_buf, rx_buf);
+	mee_spi_transfer(spi, &config, 3, tx_buf, rx_buf);
 
 	return 0;
 }
