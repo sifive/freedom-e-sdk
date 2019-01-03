6c499b707ad3864aa7f66805d6c9093813a80d3d
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-01-03 09:28:33 -0800

    Update BSPs for Arty Clocks
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- bsp/coreip-e31-arty/mee.h
+++ bsp/coreip-e31-arty/mee.h
@@ -1,7 +1,12 @@
 #ifndef ASSEMBLY
+#include <mee/drivers/fixed-clock.h>
 #include <mee/drivers/sifive,gpio0.h>
 #include <mee/drivers/sifive,uart0.h>
 #include <mee/drivers/sifive,test0.h>
+/* From clock@0 */
+asm (".weak __mee_dt_clock_0");
+struct __mee_driver_fixed_clock __mee_dt_clock_0;
+
 /* From gpio@20002000 */
 asm (".weak __mee_dt_gpio_20002000");
 struct __mee_driver_sifive_gpio0 __mee_dt_gpio_20002000;
@@ -13,6 +18,13 @@ struct __mee_driver_sifive_uart0 __mee_dt_serial_20000000;
 /* From teststatus@4000 */
 asm (".weak __mee_dt_teststatus_4000");
 struct __mee_driver_sifive_test0 __mee_dt_teststatus_4000;
+
+/* From clock@0 */
+struct __mee_driver_fixed_clock __mee_dt_clock_0 = {
+    .vtable = &__mee_driver_vtable_fixed_clock,
+    .clock.vtable = &__mee_driver_vtable_fixed_clock.clock,
+    .rate = 32500000UL,
+};
 
 /* From gpio@20002000 */
 struct __mee_driver_sifive_gpio0 __mee_dt_gpio_20002000 = {
@@ -27,7 +39,8 @@ struct __mee_driver_sifive_uart0 __mee_dt_serial_20000000 = {
     .uart.vtable = &__mee_driver_vtable_sifive_uart0.uart,
     .control_base = 536870912UL,
     .control_size = 4096UL,
-    .clock = NULL,
+/* From clock@0 */
+    .clock = &__mee_dt_clock_0.clock,
     .pinmux = NULL,
 };
 
