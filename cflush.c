fc0ab220b8f3333736bc94be0b3e6ca773fceea6
Bunnaroath Sou <bsou@sifive.com>
2019-07-11 23:48:57 -0700

    Use freedom-metal API to flush

--- cflush.c
+++ cflush.c
@@ -3,20 +3,20 @@
 
 #include <stdio.h>
 #include <metal/timer.h>
+#include <metal/cache.h>
 
 int data = 0xABBACADA;
-
-static inline void cache_flush()
-{
-    __asm__ __volatile__ (".word 0xfc000073" : : : "memory"); // CFLUSH.D.L1
-    __asm__ __volatile__ ("fence w ,r" ::: "memory");         // FENCE
-}
 
 int main (void)
 {
     int i, test, dummy;
     unsigned long long mc_count0, mc_count1;
     unsigned long long mc[2];
+
+    if (metal_dcache_l1_available(0) == 0) {
+        // abort since hardware dont support it
+        return 1;
+    }
 
     // This example demo how to use cflush (Data) L1 and use FENCE to ensure flush complete
     test = data;
@@ -25,13 +25,13 @@ int main (void)
         test = data;
         dummy = test++; // Ensure load actually complete
         metal_timer_get_cyclecount(0, &mc_count1);
-        cache_flush();
+        metal_dcache_l1_flush(0);
         mc[i] = mc_count1 - mc_count0;
     }
 
     if (mc[1] <= mc[0]) {
         printf("CFLUSH failed, pre-mc %d post-mc %d\n", mc[0], mc[1]);
-        return 1;
+        return 2;
     }
 
     // return
