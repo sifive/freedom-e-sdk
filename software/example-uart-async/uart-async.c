a52f36359596d465c17b9870634753cabb5cbf9d
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-10-03 14:16:15 -0700

    Example takes input too
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- software/example-uart-async/uart-async.c
+++ software/example-uart-async/uart-async.c
@@ -14,13 +14,18 @@ volatile int counter = 0;
 
 #define MSG_MAX_LEN 100
 char msg[MSG_MAX_LEN];
+char response[MSG_MAX_LEN];
 
-void send_callback(struct metal_uart *uart, metal_uart_callback_cause cause) {
-    if (cause != METAL_UART_TX_DONE) {
+void uart_async_cb(struct metal_uart *uart, metal_uart_callback_cause cause) {
+    if (cause == METAL_UART_RX_DONE) {
+        snprintf(response, MSG_MAX_LEN, "You entered: %s after a count of %d\r\n", msg, counter);
+        metal_uart_send_async(uart, response, strlen(response), uart_async_cb);
+    } else if (cause == METAL_UART_TX_DONE) {
+        puts("Done\n");
+        exit(0);
+    } else {
         exit(1);
     }
-
-    puts("Done transmitting\n");
 }
 
 int main() {
@@ -37,12 +42,12 @@ int main() {
 
     metal_interrupt_enable(uart_intr, uart_id);
     metal_interrupt_enable(cpu_intr, 0);
-    
+
+    puts("Please enter a 10-character message: ");
+
+    metal_uart_recv_async(uart, msg, 10, uart_async_cb);
+
     while(1) {
-        if (counter % 1000 == 0) {
-            snprintf(msg, MSG_MAX_LEN, "Counter: %d\r\n", counter);
-            metal_uart_send(uart, msg, strnlen(msg, MSG_MAX_LEN), send_callback);
-        }
         counter += 1;
     }
 
