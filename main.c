bb2e9c95c4bbfd38d68b97ad614389fb5dd27e77
Bunnaroath Sou <bsou@sifive.com>
2019-10-07 18:04:02 -0700

    Fix C99 format check

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 /* Copyright 2019 SiFive, Inc */
 /* SPDX-License-Identifier: Apache-2.0 */
 #include <stdio.h>
+#include <inttypes.h>
 #include <stdint.h>
 #include <metal/csr.h>
 
@@ -10,11 +11,11 @@ uintptr_t ms1, ms2;
     ms2 = 0xdeadbeef;
     METAL_CPU_SET_CSR(mscratch, ms2);
     METAL_CPU_GET_CSR(mscratch, ms2); 
-    printf("read 0x%x, write 0x%x, read 0x%x\n", ms1, 0xdeadbeef, ms2);
+    printf("read 0x%" PRIxPTR ", write 0x%x, read 0x%" PRIxPTR "\n", ms1, 0xdeadbeef, ms2);
     ms1 = 0xdeafabba;
     METAL_CPU_SET_CSR(0x340, ms1);
     METAL_CPU_GET_CSR(0x340, ms2); 
-    printf("read write 0x%x, read 0x%x\n", ms1, ms2);
+    printf("read write 0x%" PRIxPTR ", read 0x%" PRIxPTR "\n", ms1, ms2);
     return 0;
 }
 
