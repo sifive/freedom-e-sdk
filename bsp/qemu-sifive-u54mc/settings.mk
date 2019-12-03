4cc2f1a9bbfe0da5d8ac92181618399c2e79d3c2
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-12-03 13:33:05 -0800

    Regen BSPs
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- bsp/qemu-sifive-u54mc/settings.mk
+++ bsp/qemu-sifive-u54mc/settings.mk
@@ -1,12 +1,11 @@
-# Copyright 2019 SiFive, Inc #
-# SPDX-License-Identifier: Apache-2.0 #
-# ----------------------------------- #
-# ----------------------------------- #
+# Copyright (C) 2020 SiFive Inc
+# SPDX-License-Identifier: Apache-2.0
 
-RISCV_ARCH=rv64imafdcsu
-RISCV_ABI=lp64d
-RISCV_CMODEL=medany
+RISCV_ARCH = rv64imafdcsu
+RISCV_ABI = lp64d
+RISCV_CMODEL = medany
+RISCV_SERIES = None
 
-TARGET_TAGS=qemu
-TARGET_DHRY_ITERS=20000000
-TARGET_CORE_ITERS=5000
+TARGET_TAGS = qemu
+TARGET_DHRY_ITERS = 20000000
+TARGET_CORE_ITERS = 5000
--- bsp/sifive-hifive-unleashed/settings.mk
+++ bsp/sifive-hifive-unleashed/settings.mk
@@ -1,13 +1,11 @@
-# Copyright 2019 SiFive, Inc #
-# SPDX-License-Identifier: Apache-2.0 #
-# ----------------------------------- #
-# ----------------------------------- #
+# Copyright (C) 2020 SiFive Inc
+# SPDX-License-Identifier: Apache-2.0
 
-RISCV_ARCH=rv64imafdc
-RISCV_ABI=lp64d
-RISCV_CMODEL=medany
-RISCV_SERIES=sifive-5-series
+RISCV_ARCH = rv64imafdc
+RISCV_ABI = lp64d
+RISCV_CMODEL = medany
+RISCV_SERIES = sifive-5-series
 
-TARGET_TAGS=board openocd
-TARGET_DHRY_ITERS=20000000
-TARGET_CORE_ITERS=5000
+TARGET_TAGS = board openocd
+TARGET_DHRY_ITERS = 20000000
+TARGET_CORE_ITERS = 5000
