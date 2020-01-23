2be7d359af832b16da3fb57fd14f38fae7dc066a
Nathaniel Graff <nathaniel.graff@sifive.com>
2020-01-23 11:28:02 -0800

    Cache all pip dependencies in pip-cache submodule
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- scripts/virtualenv.mk
+++ scripts/virtualenv.mk
@@ -2,7 +2,15 @@
 .PHONY: virtualenv
 virtualenv: venv/.stamp
 
-venv/.stamp: venv/bin/activate pip-cache requirements.txt
+# Just in case the cached dependencies fail, users can install Python packages
+# from the Python Package Index by running `make virtualenv-from-pypi`
+
+.PHONY: virtualenv-from-pypi
+virtualenv-from-pypi: venv/bin/activate requirements.txt
+	. $< && pip install --upgrade pip
+	. $< && pip install -r requirements.txt
+
+venv/.stamp: venv/bin/activate requirements.txt
 	. $< && pip install --no-index --find-links pip-cache --upgrade pip
 	. $< && pip install --no-index --find-links pip-cache -r requirements.txt
 	touch $@
@@ -14,22 +22,18 @@ venv/bin/activate:
 # that all are platform-indepentent.
 #
 # The result should be that when freedom-e-sdk is cloned, the virtualenv is created simply
-# by caching all dependencies in pip-cache and then installing them.
+# by installing all cached dependencies from the submodule
 #
-# This allows freedom-e-sdk to be packaged for customer deliveries with all dependencies
-# pre-cached and available for offline installation. Simply run `make pip-cache` or any build
-# that depends on the `pip-cache` or `virtualenv` targets and then include the pip-cache directory
-# in the archived SDK.
+# The pip-cache can be updated when requirements.txt changes by running `make update-pip-cache`,
+# commiting the changes to the submodule, and bumping the submodule.
 
-pip-cache: requirements.txt
-	python3 -m venv venv
-	mkdir -p $@
-	. venv/bin/activate && pip install --upgrade pip
-	. venv/bin/activate && pip download -d $@ -r requirements.txt --platform any --no-deps
+.PHONY: update-pip-cache
+update-pip-cache: requirements.txt
+	./pip-cache/download-python-dependencies.py $<
 
 .PHONY: clean-pip-cache
 clean-pip-cache:
-	-rm -rf pip-cache
+	-rm -rf pip-cache/*.whl pip-cache *.tar.gz
 
 .PHONY: clean-virtualenv
 clean-virtualenv:
