67ed5872fd69a7376c21437fadf570d01090fcce
Emmanuel Puerto <emmanuel.puerto@sifive.com>
2020-02-06 18:47:04 +0100

    Change value of configTICK_RATE_HZ for better understanding

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -47,7 +47,14 @@
 #define configUSE_IDLE_HOOK			0
 #define configUSE_TICK_HOOK			0
 #define configCPU_CLOCK_HZ			( MTIME_RATE_HZ ) 
-#define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
+/* 
+ * configTICK_RATE_HZ indicate the frequency in HZ of the Tick.
+ * On some CPU having a configTICK_RATE_HZ too high could induce scheduling time consuption
+ * to high, then the task will not have time to run.
+ * 
+ * configTICK_RATE_HZ = 100 means a Tick every 10ms.
+ */
+#define configTICK_RATE_HZ			( ( TickType_t ) 100 )
 #define configMAX_PRIORITIES			( 7 )
 #define configMINIMAL_STACK_SIZE		( ( size_t ) 128 + PORT_CONTEXT_lastIDX )
 #define configAPPLICATION_ALLOCATED_HEAP 	0
