ca48d0d370ec7a50896e64252391b9e61d4dab1f
Emmanuel Puerto <emmanuel.puerto@sifive.com>
2020-03-10 16:46:40 +0100

    first commit

---
+++ Bridge_Freedom-metal_FreeRTOS.c
@@ -0,0 +1,222 @@
+/* Copyright 2019 SiFive, Inc */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+/* FreeRTOS kernel includes. */
+#include <FreeRTOS.h>
+
+/* Freedom metal includes. */
+#include <metal/machine.h>
+#include <metal/machine/platform.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#if( configUSE_SEGGER_SYSTEMVIEW == 1 )
+# include "SEGGER_SYSVIEW_FreeRTOS.h"
+#endif
+
+static __attribute__ ((aligned(16))) StackType_t xISRStack[ configMINIMAL_STACK_SIZE ] __attribute__ ((section (".heap"))) = { 0 };
+__attribute__ ((aligned(4))) uint8_t ucHeap[ configTOTAL_HEAP_SIZE ] __attribute__ ((section (".heap")));
+
+
+__attribute__((constructor)) static void FreeRTOS_init(void);
+#ifdef SEGGER_SYSTEMVIEW
+__attribute__((constructor)) static void SEGGER_SysView_init(void);
+#endif
+
+__attribute__((constructor)) static void FreeRTOS_init(void)
+{
+	struct metal_cpu *cpu;
+	struct metal_interrupt *cpu_intr;
+	extern BaseType_t xPortFreeRTOSInit( StackType_t xIsrStack );
+	
+	const char * const pcErrorMsg = "No External controller\n";
+
+	/* Remove compiler warning about unused parameter. */
+	( void ) pcErrorMsg;
+
+  /*
+   * Initilize freedom-metal interrupt managment.
+   *   Its SHOULD be made before calling xPortFreeRTOSInit because
+   *   the interrupt/exeception handler MUST be the freertos handler.
+   */
+	cpu = metal_cpu_get(metal_cpu_get_current_hartid());
+	if (cpu == NULL)
+	{
+		return;
+	}
+
+	cpu_intr = metal_cpu_interrupt_controller(cpu);
+	if (cpu_intr == NULL)
+	{
+		return;
+	}
+	metal_interrupt_init(cpu_intr);
+
+	if (metal_interrupt_enable(cpu_intr, 0) == -1)
+	{
+		return;
+	}
+
+#ifdef METAL_RISCV_PLIC0
+	{
+		struct metal_interrupt *plic;
+
+		// Check if this target has a plic. If not gracefull exit
+		plic = metal_interrupt_get_controller(METAL_PLIC_CONTROLLER, 0);
+		if (plic == NULL) {
+			write( STDOUT_FILENO, pcErrorMsg, strlen( pcErrorMsg ) );
+
+			for( ;; );
+		} 
+		metal_interrupt_init(plic);
+	}
+#endif
+
+#ifdef METAL_SIFIVE_CLIC0
+	{
+	    struct metal_interrupt *clic;
+
+		// Check we this target has a plic. If not gracefull exit
+		clic = metal_interrupt_get_controller(METAL_CLIC_CONTROLLER, 0);
+		if (clic == NULL) {
+			write( STDOUT_FILENO, pcErrorMsg, strlen( pcErrorMsg ) );
+
+			for( ;; );
+		} 
+		metal_interrupt_init(clic);
+	}
+#endif
+
+	/*
+	* Call xPortFreeRTOSInit in order to set xISRTopStack
+	*/
+	if ( 0 != xPortFreeRTOSInit((StackType_t)&( xISRStack[ ( configMINIMAL_STACK_SIZE & ~portBYTE_ALIGNMENT_MASK ) - 1 ] ))) {
+		_exit(-1);
+	}
+}
+
+
+void FreedomMetal_InterruptHandler( portUBASE_TYPE hartid, portUBASE_TYPE mcause, portUBASE_TYPE mtvec )
+{	
+    int id;
+    void *priv;
+    struct __metal_driver_riscv_cpu_intc *intc;
+    struct __metal_driver_cpu *cpu;
+
+    cpu = __metal_cpu_table[hartid];
+
+    if ( cpu ) {
+
+        intc = (struct __metal_driver_riscv_cpu_intc *)
+          __metal_driver_cpu_interrupt_controller((struct metal_cpu *)cpu);
+        id = mcause & METAL_MCAUSE_CAUSE;
+
+		if ((id < METAL_INTERRUPT_ID_LC0) ||
+		   ((mtvec & METAL_MTVEC_MASK) == METAL_MTVEC_DIRECT)) {
+		    priv = intc->metal_int_table[id].exint_data;
+			if (intc->metal_int_table[id].handler != NULL)
+		    	intc->metal_int_table[id].handler(id, priv);
+			goto cleanup;
+		}
+		if ((mtvec & METAL_MTVEC_MASK) == METAL_MTVEC_CLIC) {
+		    uintptr_t mtvt;
+		    metal_interrupt_handler_t mtvt_handler;
+
+		    __asm volatile ("csrr %0, mtvt" : "=r"(mtvt));
+		    priv = intc->metal_int_table[METAL_INTERRUPT_ID_SW].sub_int;
+		    mtvt_handler = (metal_interrupt_handler_t)mtvt;
+			if (mtvt_handler != NULL)
+		    	mtvt_handler(id, priv);
+			goto cleanup;
+		}
+	}
+
+cleanup:
+	return;
+}
+
+void FreedomMetal_ExceptionHandler( void )
+{
+    int id;
+    uintptr_t mcause, hartid;
+    struct __metal_driver_riscv_cpu_intc *intc;
+    struct __metal_driver_cpu *cpu;
+	
+    __asm__ __volatile__ ("csrr %0, mhartid" : "=r"(hartid));
+    cpu = __metal_cpu_table[hartid];
+
+    if ( cpu ) {
+        intc = (struct __metal_driver_riscv_cpu_intc *)
+          __metal_driver_cpu_interrupt_controller((struct metal_cpu *)cpu);
+
+        __asm__ __volatile__ ("csrr %0, mcause" : "=r"(mcause));
+        id = mcause & METAL_MCAUSE_CAUSE;
+
+		configASSERT( id < METAL_ECALL_U_EXCEPTION_CODE );
+
+		if (id < METAL_ECALL_U_EXCEPTION_CODE) {
+			if (intc->metal_exception_table[id] != NULL)
+				intc->metal_exception_table[id]((struct metal_cpu *)cpu, id);
+		}
+	}
+
+	for( ;; ); // return is dangerous, we just got a critical exception.
+	return;
+}
+
+#if( configUSE_SEGGER_SYSTEMVIEW == 1 )
+__attribute__((constructor)) static void SEGGER_SysView_init(void)
+{
+	SEGGER_SYSVIEW_Conf();
+  SEGGER_SYSVIEW_Start();
+}
+
+U32 SEGGER_SYSVIEW_X_GetInterruptId(void) {
+#if (__riscv_xlen == 64)
+  uintptr_t mcause;
+
+  __asm__ __volatile__ ("csrr %0, mcause" : "=r"(mcause));
+
+  if (mcause & 0x8000000000000000)
+    mcause = mcause & 0x7FFFFFFFFFFFFFFF;
+  else
+    mcause = mcause & 0x8000000000000000;
+
+  return (U32)mcause;
+#elif (__riscv_xlen == 32)
+  uintptr_t mcause;
+
+  __asm__ __volatile__ ("csrr %0, mcause" : "=r"(mcause));
+
+  if (mcause & 0x80000000)
+    mcause = mcause & 0x7FFFFFFF;
+  else
+    mcause = mcause & 0x80000000;
+
+  return (U32)mcause;
+#endif
+}
+
+#ifndef configCLINT_BASE_ADDRESS
+  #error No CLINT Base Address defined
+#endif
+
+U32 SEGGER_SYSVIEW_X_GetTimestamp(void) {
+#if (__riscv_xlen == 64)
+  return (U32)(*(( uint64_t * volatile ) ( configCLINT_BASE_ADDRESS + 0xBFF8) ));
+#elif (__riscv_xlen == 32) 
+  uint32_t lo, hi;
+
+    /* Guard against rollover when reading */
+    do {
+        hi = *(( uint32_t * volatile ) ( configCLINT_BASE_ADDRESS + 0xBFFC) );
+        lo = *(( uint32_t * volatile ) ( configCLINT_BASE_ADDRESS + 0xBFF8) );
+    } while ( *(( uint32_t * volatile ) ( configCLINT_BASE_ADDRESS + 0xBFFC)) != hi);
+
+	return (U32)lo;
+#endif
+}
+
+#endif
---
+++ Makefile
@@ -0,0 +1,121 @@
+# Copyright 2019 SiFive, Inc #
+# SPDX-License-Identifier: Apache-2.0 #
+
+# Provide a default for no verbose output
+HIDE ?= @
+
+PROGRAM ?= example-freertos-blinky-pmp
+
+OBJ_DIR ?= ./$(CONFIGURATION)/build
+
+C_SOURCES = $(wildcard *.c)
+
+#     Add local include 
+override CFLAGS  += -I./
+
+# ----------------------------------------------------------------------
+# Build List of Object according C/CPP/S source file list
+# ----------------------------------------------------------------------
+_C_OBJ_FILES   += $(C_SOURCES:%.c=${OBJ_DIR}/%.o)
+_CXX_OBJ_FILES += $(CXX_SOURCES:%.cpp=${OBJ_DIR}/%.o)
+
+_asm_s := $(filter %.s,$(S_SOURCES))
+_asm_S := $(filter %.S,$(S_SOURCES))
+_ASM_OBJ_FILES := $(_asm_s:%.s=${OBJ_DIR}/%.o) $(_asm_S:%.S=${OBJ_DIR}/%.o)
+
+OBJS += ${_C_OBJ_FILES}
+OBJS += ${_CXX_OBJ_FILES}
+OBJS += ${_ASM_OBJ_FILES}
+
+# ----------------------------------------------------------------------
+# Add custom flags for link
+# ----------------------------------------------------------------------
+# Reduce default size of the stack and the heap
+#
+_ADD_LDFLAGS  += -Wl,--defsym,__stack_size=0x200
+_ADD_LDFLAGS  += -Wl,--defsym,__heap_size=0x200
+
+# ----------------------------------------------------------------------
+# Add custom flags for FreeRTOS
+# ----------------------------------------------------------------------
+FREERTOS_SOURCE_PATH ?= ../../FreeRTOS-metal
+FREERTOS_DIR = $(abspath $(FREERTOS_SOURCE_PATH))
+include $(FREERTOS_DIR)/scripts/FreeRTOS.mk
+
+export portHANDLE_INTERRUPT=FreedomMetal_InterruptHandler
+export portHANDLE_EXCEPTION=FreedomMetal_ExceptionHandler
+export FREERTOS_CONFIG_DIR = $(abspath ./)
+export MTIME_CTRL_ADDR=0x2000000
+ifeq ($(TARGET),sifive-hifive-unleashed)
+        export MTIME_RATE_HZ=1000000
+else
+        export MTIME_RATE_HZ=32768
+endif
+export HEAP = 4
+
+override CFLAGS +=      $(foreach dir,$(FREERTOS_INCLUDES),-I $(dir)) \
+                                        -I $(FREERTOS_CONFIG_DIR) \
+                                        -I $(join $(abspath  $(BUILD_DIRECTORIES)),/FreeRTOS/include)
+
+override LDLIBS += -lFreeRTOS
+override LDFLAGS += -L$(join $(abspath  $(BUILD_DIRECTORIES)),/FreeRTOS/lib)
+
+
+# ----------------------------------------------------------------------
+# Update LDLIBS
+# ----------------------------------------------------------------------
+FILTER_PATTERN = -Wl,--end-group
+override LDLIBS := $(filter-out $(FILTER_PATTERN),$(LDLIBS)) -Wl,--end-group
+
+ifneq ($(filter rtl,$(TARGET_TAGS)),)
+override CFLAGS += -D_RTL_
+endif
+
+# ----------------------------------------------------------------------
+# Compile Object Files From Assembly
+# ----------------------------------------------------------------------
+$(OBJ_DIR)/%.o: %.S
+	@echo "Assemble: $<"
+	$(HIDE)$(CC) -o $@ $(ASFLAGS) $(CPPFLAGS) $(_COMMON_CFLAGS) $<
+
+# ----------------------------------------------------------------------
+# Compile Object Files From C
+# ----------------------------------------------------------------------
+$(OBJ_DIR)/%.o: %.c
+	@echo "Compile: $<"
+	$(HIDE)$(CC) -c -o $@ $(CFLAGS) $(CPPFLAGS) $(CFLAGS_COMMON) $(_COMMON_CFLAGS) $<
+
+# ----------------------------------------------------------------------
+# Compile Object Files From CPP
+# ----------------------------------------------------------------------
+$(OBJ_DIR)/%.o: %.cpp
+	@echo "Compile: $<"
+	$(HIDE)$(CXX) -c -o $@ $(CXXFLAGS) $(CPPFLAGS) $(CFLAGS_COMMON) $(_COMMON_CFLAGS) $<
+
+# ----------------------------------------------------------------------
+# create dedicated directory for Object files
+# ----------------------------------------------------------------------
+BUILD_DIRECTORIES = \
+	$(OBJ_DIR) 
+
+# ----------------------------------------------------------------------
+# Build rules
+# ----------------------------------------------------------------------
+$(BUILD_DIRECTORIES):
+	mkdir -p $@
+
+directories: $(BUILD_DIRECTORIES)
+
+libfreertos:
+	make -f Makefile -C $(FREERTOS_DIR) BUILD_DIR=$(join $(abspath  $(BUILD_DIRECTORIES)),/FreeRTOS) libFreeRTOS.a VERBOSE=$(VERBOSE) PMP=ENABLE
+
+$(PROGRAM): \
+	directories \
+	libfreertos \
+	$(OBJS)
+	@echo "Build: $@"
+	$(CC) $(CFLAGS) $(LDFLAGS) $(_ADD_LDFLAGS) $(OBJS) $(LOADLIBES) $(LDLIBS) -o $@
+
+clean::
+	rm -rf $(BUILD_DIRECTORIES)
+	rm -f $(PROGRAM) $(PROGRAM).hex
