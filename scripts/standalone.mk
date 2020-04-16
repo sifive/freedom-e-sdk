aa4391ef7088941ab99937f88040f340efe1de62
Kevin Mills <43148204+sifivekevin@users.noreply.github.com>
2020-04-16 12:58:35 -0700

    Make standalone projects use a project-local venv

--- scripts/standalone.mk
+++ scripts/standalone.mk
@@ -10,8 +10,8 @@ SRC_DIR ?= $(abspath src)
 export FREERTOS_SOURCE_PATH = $(abspath FreeRTOS-metal)
 # FREERTOS_SOURCE_PATH sets the path to the SEGGER SystemView source directory
 export SYSTEMVIEW_SOURCE_PATH = $(abspath Segger_SystemView-metal)
-# Set FREERTOS_METAL_VENV_PATH to re-use Freedom E SDK's virtualenv
-export FREERTOS_METAL_VENV_PATH = $(FREEDOM_E_SDK_VENV_PATH)
+# Set FREERTOS_METAL_VENV_PATH to use a project-local virtualenv
+export FREERTOS_METAL_VENV_PATH = venv
 
 #############################################################
 # BSP loading
