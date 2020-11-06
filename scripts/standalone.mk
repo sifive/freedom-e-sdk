1a04a72814425b2a3e05782a55273bd08ea5a3b7
cgsfv <cgsfv@users.noreply.github.com>
2020-11-05 18:10:36 -0800

    Adding list-all-info target

--- scripts/standalone.mk
+++ scripts/standalone.mk
@@ -255,14 +255,14 @@ RISCV_XCFLAGS += -DMETAL_WAIT_CYCLE=$(TARGET_INTR_WAIT_CYCLE)
 #############################################################
 # Freedom Studio
 #############################################################
-#.PHONY: list-all-info
-#list-all-info:
-#	@echo e-sdk-tags: $(E_SDK_TAGS)
-#	@echo e-sdk-reqs: $(E_SDK_REQS)
-#	@echo riscv-arch: $(RISCV_ARCH)
-#	@echo target-tags: $(TARGET_TAGS)
-#	@echo riscv-reqs: $(RISCV_REQS)
-#	@echo program-tags: $(PROGRAM_TAGS)
+.PHONY: list-all-info
+list-all-info:
+	@echo e-sdk-tags: $(E_SDK_TAGS)
+	@echo e-sdk-reqs: $(E_SDK_REQS)
+	@echo riscv-arch: $(RISCV_ARCH)
+	@echo target-tags: $(TARGET_TAGS)
+	@echo riscv-reqs: $(RISCV_REQS)
+	@echo program-tags: $(PROGRAM_TAGS)
 
 #############################################################
 # Software
